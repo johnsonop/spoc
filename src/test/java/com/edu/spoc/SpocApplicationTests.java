3348cadfd20e88bd9f06289239e0caeb527363d2
wanjc <924235514@qq.com>
2019-02-14 13:23:06 +0800

    创建项目

---
+++ src/test/java/com/edu/spoc/SpocApplicationTests.java
@@ -0,0 +1,17 @@
+package com.edu.spoc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpocApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
