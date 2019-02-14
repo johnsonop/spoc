b251415dac5ae572cace3a870cbf9933ce23c287
wanjc <924235514@qq.com>
2019-02-14 13:28:22 +0800

    create project

---
+++ src/main/java/com/edu/spoc/SpocApplication.java
@@ -0,0 +1,14 @@
+package com.edu.spoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpocApplication.class, args);
+    }
+
+}
+
